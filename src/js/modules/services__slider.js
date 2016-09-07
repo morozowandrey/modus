a17acad4eea746a6fd39d674bb139463d2e6ad34
Andrew Morozow <andrey940@gmail.com>
2016-09-07 19:34:42 +0300

    servises page done

--- src/js/modules/services__slider.js
+++ src/js/modules/services__slider.js
@@ -1,35 +1,16 @@
-	$(window).load(function(){	
-		// function Slider(width_li,margin_right_li,col_view_img){
-  //           var step=width_li+margin_right_li,
-  //               slider_box_with=col_view_img*step-margin_right_li,
-  //               $col_img=$("#slider_box>ul>li").length,
-  //               col_main_left=0,
-  //               max_col_main_left=$col_img*step;
-  //           $("#slider_box").width(slider_box_with);
-  //           $("#slider_box>ul>li").width(width_li).css("margin-right",margin_right_li);
-  //           $("#left_nav").click(function(){
-  //               if(col_main_left==0){
-  //                   col_main_left=-max_col_main_left+col_view_img*step;
-  //               } else{
-  //                   col_main_left=col_main_left+step;
-  //               }
-  //               $("#slider_box>ul").css("margin-left",col_main_left+"px");
-  //           });
-  //           $("#right_nav").click(function(){
-  //           	if(-col_main_left==max_col_main_left-col_view_img*step){
-  //           	    col_main_left=0;
-  //           	} else{
-  //           	    col_main_left=col_main_left-step;
-  //           	}
-  //           	$("#slider_box>ul").css("margin-left",col_main_left+"px");
-                
-  //           });
-  //       }
-  //       $(Slider(200,10,6));
+	$(window).load(function(){
 
         $('.slider-box').slick({
           slidesToShow: 6,
           slidesToScroll: 1
         });
+	if(window.matchMedia('(max-width: 768px)').matches)
+	{
+	    $('.slider-box').slick({
+	              slidesToShow: 4,
+	              slidesToScroll: 1
+	            });
+	}
     })
+
 
